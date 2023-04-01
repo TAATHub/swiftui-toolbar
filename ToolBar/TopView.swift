98a84c13866664961290e7f9eb2ae6290dd96fd2
TAAT <20purpleblue10@gmail.com>
2023-04-01 21:27:39 +0900

    [update] principalIconを追加、コードを整理

--- ToolBar/TopView.swift
+++ ToolBar/TopView.swift
@@ -21,41 +21,64 @@ struct TopView: View {
                         NotificationView()
                     }
                 }
+                // ナビゲーションバーに要素を配置
                 .toolbar {
                     ToolbarItem(placement: .navigationBarLeading) {
-                        Button {
-                            path.append(.account)
-                        } label: {
-                            Image(systemName: "person.circle")
-                                .fontWeight(.bold)
-                        }
+                        AccountIcon()
+                    }
+                    
+                    ToolbarItem(placement: .principal) {
+                        principalIcon()
                     }
                     
                     ToolbarItem(placement: .navigationBarTrailing) {
-                        ZStack(alignment: .topTrailing) {
-                            Button {
-                                path.append(.notification)
-                            } label: {
-                                Image(systemName: "bell")
-                                    .fontWeight(.bold)
-                            }
-                            
-                            Group {
-                                Text("2")
-                                    .frame(minWidth: 14)
-                                    .padding(3)
-                                    .font(.system(size: 12))
-                                    .foregroundColor(.white)
-                                    .background(.blue)
-                                    .cornerRadius(40)
-                                    .offset(x: 6, y: -4)
-                            }
-                            .allowsHitTesting(false)
-                        }
+                        notificationIcon()
                     }
                 }
         }
+        // ナビゲーションバー全体にtintColorを指定
         .tint(.init(red: 0.2, green: 0.2, blue: 0.2))
+    }
+    
+    private func AccountIcon() -> some View {
+        Button {
+            path.append(.account)
+        } label: {
+            Image(systemName: "person.circle")
+                .fontWeight(.bold)
+        }
+    }
+    
+    private func principalIcon() -> some View {
+        Image("taat")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 36, height: 36)
+            .clipShape(Circle())
+    }
+    
+    private func notificationIcon() -> some View {
+        // アイコンとバッジを右上揃えにする
+        ZStack(alignment: .topTrailing) {
+            Button {
+                path.append(.notification)
+            } label: {
+                Image(systemName: "bell")
+                    .fontWeight(.bold)
+            }
+            
+            Text("2")
+                .frame(minWidth: 14)
+                .padding(3)
+                .font(.system(size: 12))
+                .foregroundColor(.white)
+                .background(.blue)
+                .cornerRadius(40)
+                // バッジの位置をアイコンに対してやや右上にずらす
+                .offset(x: 6, y: -4)
+                // バッジ部分でのタップイベントをアイコンに透過させる
+                .allowsHitTesting(false)
+        }
     }
 }
 
